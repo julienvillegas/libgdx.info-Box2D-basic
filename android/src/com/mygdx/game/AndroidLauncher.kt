aec276365bca2f27cb52350c39eabaff3bc09d8a
Kondor75 <sasha.2000.potapov@mail.ru>
2018-01-15 18:29:26 +0300

    fixed bug after deleting tests

--- android/src/com/mygdx/game/AndroidLauncher.kt
+++ android/src/com/mygdx/game/AndroidLauncher.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
-import com.mygdx.game.Tests.Bridge
-import com.mygdx.game.Tests.LibGDXTest
-import com.mygdx.game.Tests.SimpleTest
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
